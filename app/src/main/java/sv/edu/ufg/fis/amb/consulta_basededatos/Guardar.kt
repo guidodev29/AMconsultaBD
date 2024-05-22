67d28343dfe9dd0b72033805d1fc90ea3c933a84
unknown <UFG@CC6-07.ufg.local>
2024-05-21 20:05:56 -0600

    Practica con toast falta insert

--- app/src/main/java/sv/edu/ufg/fis/amb/consulta_basededatos/Guardar.kt
+++ app/src/main/java/sv/edu/ufg/fis/amb/consulta_basededatos/Guardar.kt
@@ -2,12 +2,13 @@ package sv.edu.ufg.fis.amb.consulta_basededatos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import sv.edu.ufg.fis.amb.consulta_basededatos.databinding.ActivityGuardarBinding
 
 class Guardar : AppCompatActivity() {
 
     private lateinit var binding: ActivityGuardarBinding
-    //private lateinit var db
+    private lateinit var db: BaseDeDatos
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,5 +16,14 @@ class Guardar : AppCompatActivity() {
         binding=ActivityGuardarBinding.inflate(layoutInflater)
         //modificamos lo de abajo
         setContentView(binding.root)
+
+        db = BaseDeDatos(this)
+        binding.btnGuardar.setOnClickListener{
+            val mensaje = binding.txtData.text.toString()
+            val registro = Registro(mensaje)
+            db.insertarMensaje(registro)
+            finish()
+            Toast.makeText(this, "SE HA GUARDADO EL MENSAJE",Toast.LENGTH_LONG).show()
+        }
     }
 }
